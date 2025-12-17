feeb062274c3d07c2665bca031dc1f28f5708868
ishak <i.kouici@insta.fr>
2025-12-17 14:01:13 +0100

    ad header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./composants/header";
 
 function App() {
   return (
     <div className="App">
+        <Header/>
      <h1>Mon application codé avec 0 IA</h1>
     </div>
   );
---
+++ src/composants/header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <div className="App">
+            <lu>
+                <a href={''}><li>Acceuil</li></a>
+                <a href={''}><li>Boutique</li></a>
+                <a href={''}><li>Contacter le service client</li></a>
+            </lu>
+        </div>
+    );
+}
+
+export default Header;
